183697196bc8ba5188445b4705254ccad4313acd
Viet Hoang <noneedforlife.0612@gmail.com>
2024-04-21 15:51:58 +0700

    # add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="vn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Không tìm thấy trang</title>
+</head>
+<body>
+    <div style="display: flex; justify-content: center">
+        <img src="{{ asset('images/404.gif') }}" alt="">
+    </div>
+</body>
+</html>
