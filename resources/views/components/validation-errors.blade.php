efdf01a2c49933fed05ad6d97e8ec02ca2d44380
Viet Hoang <noneedforlife.0612@gmail.com>
2024-05-03 16:14:44 +0700

    update login

--- resources/views/components/validation-errors.blade.php
+++ resources/views/components/validation-errors.blade.php
@@ -1,6 +1,6 @@
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">{{ __('Vui lòng kiểm tra lại email hoặc mật khẩu') }}</div>
+        <div class="font-medium text-red-600">{{ __('Vui lòng kiểm tra thông tin nhập!') }}</div>
 
         <!-- <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
