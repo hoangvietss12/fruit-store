27ae5c24a1c6304ec013a4640dc8d996d64f8475
Viet Hoang <noneedforlife.0612@gmail.com>
2024-05-20 13:51:01 +0700

    fix custom js in admin

--- public/admin/assets/js/custom.js
+++ public/admin/assets/js/custom.js
@@ -1,33 +1,43 @@
-// click submit
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btn-report-search").addEventListener("click", function() {
-        document.getElementById("form-report").submit();
-    });
-});
+    // click submit
+    const btnReportSearch = document.getElementById('btn-report-search');
 
-// click add more event
-document.getElementById('btn-add-more').addEventListener('click', function(event) {
-    event.preventDefault();
-    var productGroup = document.querySelector('.product-group').cloneNode(true);
-    var btnRemove = document.createElement('button');
-    btnRemove.classList.add('btn', 'btn-danger', 'btn-remove');
-    btnRemove.type = 'button';
-    btnRemove.textContent = 'Xóa';
-    productGroup.appendChild(btnRemove);
-
-
-    var inputs = productGroup.querySelectorAll('input, textarea, select');
-    inputs.forEach(function(input) {
-        input.value = '';
-    });
-
-    var btnGroup = document.querySelector('#btn-add-more');
-    btnGroup.parentNode.insertBefore(productGroup, btnGroup);
-});
-
-// click remove product-group
-document.addEventListener('click', function(e) {
-    if (e.target && e.target.classList.contains('btn-remove')) {
-        e.target.closest('.product-group').remove();
+    if(btnReportSearch) {
+        btnReportSearch.addEventListener("click", function() {
+            document.getElementById("form-report").submit();
+        });
     }
+
+    // click add more event
+    const btnAddMore = document.getElementById('btn-add-more');
+
+    if(btnAddMore) {
+        btnAddMore.addEventListener('click', function(event) {
+            event.preventDefault();
+            var productGroup = document.querySelector('.product-group').cloneNode(true);
+            var btnRemove = document.createElement('button');
+            btnRemove.classList.add('btn', 'btn-danger', 'btn-remove');
+            btnRemove.type = 'button';
+            btnRemove.textContent = 'Xóa';
+            productGroup.appendChild(btnRemove);
+
+
+            var inputs = productGroup.querySelectorAll('input, textarea, select');
+            inputs.forEach(function(input) {
+                input.value = '';
+            });
+
+            var btnGroup = document.querySelector('#btn-add-more');
+            btnGroup.parentNode.insertBefore(productGroup, btnGroup);
+        });
+    }
+
+    // click remove product-group
+    document.addEventListener('click', function(e) {
+        if (e.target && e.target.classList.contains('btn-remove')) {
+            e.target.closest('.product-group').remove();
+        }
+    });
 });
+
+
