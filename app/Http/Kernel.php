d8e6feffb80704de8fe6b2e996dc082105856f2e
Viet Hoang <noneedforlife.0612@gmail.com>
2024-05-27 21:25:15 +0700

    fix path

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Illuminate\Http\Middleware\TrustProxies::class,
+            \App\Http\Middleware\RedirectIfNotHttps::class,
         ],
 
         'api' => [
