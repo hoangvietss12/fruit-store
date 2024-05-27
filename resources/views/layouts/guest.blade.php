1b86ac5da2db29a2952c49e30dd4880596ce0996
Viet Hoang <noneedforlife.0612@gmail.com>
2024-05-27 20:40:30 +0700

    update guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['public/build/assets/app-14204e20.css', 'public/build/assets/app-73f9d153.js'])
 
         <!-- Styles -->
         @livewireStyles
